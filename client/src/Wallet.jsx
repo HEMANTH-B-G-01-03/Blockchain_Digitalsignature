aaca0b197c0ae05da325447f0f77ae58b5e6012f
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2025-07-10 21:29:19 +0530

    money adding to account

---
+++ client/src/Wallet.jsx
@@ -0,0 +1,52 @@
+import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
+
+
+function Wallet({
+  address,
+  setAddress,
+  balance,
+  setBalance,
+  privateKey,
+  setPrivateKey,
+}) {
+
+  async function onChange(evt) {
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+    const address = toHex(secp.secp256k1.getPublicKey(privateKey, false));
+    setAddress(address);
+
+    if (address) {
+      const {
+        data: { balance },
+      } = await server.get(`balance/${address}`);
+      setBalance(balance);
+    } else {
+      setBalance(0);
+    }
+  }
+
+  return (
+    <div className="container wallet">
+      <h1>Your Wallet</h1>
+
+      <label>
+        Private Key
+        <input
+          placeholder="Type in a private key"
+          value={privateKey}
+          onChange={onChange}
+        ></input>
+      </label>
+
+      <label className="address">
+        Address: {address.slice(0, 15)}...
+      </label> 
+      <div className="balance">Balance: {balance}</div>
+    </div>
+  );
+}
+
+export default Wallet;
