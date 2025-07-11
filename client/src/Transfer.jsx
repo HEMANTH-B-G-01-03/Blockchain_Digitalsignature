52fafaadf6d4a99967362063d4072f5be08bf778
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2025-07-11 11:09:55 +0530

    wallet transfering client

---
+++ client/src/Transfer.jsx
@@ -0,0 +1,69 @@
+import * as secp from "ethereum-cryptography/secp256k1";
+import { utf8ToBytes } from "ethereum-cryptography/utils.js";
+import { keccak256 } from "ethereum-cryptography/keccak";
+
+import { useState } from "react";
+import server from "./server";
+
+function Transfer({ address, setBalance, privateKey }) {
+  const [sendAmount, setSendAmount] = useState("");
+
+  const [recipient, setRecipient] = useState("");
+
+  const setValue = (setter) => (evt) => setter(evt.target.value);
+    
+  async function transfer(evt) {
+    evt.preventDefault();
+     //Generating the message hash
+     const msgHash = keccak256(utf8ToBytes(sendAmount));
+     //
+       const signature = secp.secp256k1.sign(msgHash, privateKey);
+  
+        const isSigned = secp.secp256k1.verify(signature, msgHash, address);
+        console.log(isSigned)
+    try {
+      const {
+        data: { balance },
+      } = await server.post(`send`, {
+        sender: address,
+        amount: parseInt(sendAmount),
+        isSigned: isSigned,
+       recipient
+      });
+     setBalance(balance);
+    } catch (ex) {
+      console.log(ex.response.data.message);
+    }  
+    
+  
+  }
+
+ 
+  return (
+    <form className="container transfer" onSubmit={transfer}>
+      <h1>Send Transaction</h1>
+
+      <label>
+        Send Amount
+        <input
+          placeholder="1, 2, 3..."
+          value={sendAmount}
+          onChange={setValue(setSendAmount)}
+        ></input>
+      </label>
+
+      <label>
+        Recipient
+        <input
+          placeholder="Type an address, for example: 0x2"
+          value={recipient}
+          onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+
+      <input type="submit" className="button" value="Transfer" />
+    </form>
+  );
+}
+
+export default Transfer;
